a296a239de203bce72a728818e024fcb0f825d56
hoa.duonggia <giahoa.duong@officience.com>
2023-07-31 11:37:44 +0700

    useReducer

--- src/components/tictactoe/Game.js
+++ src/components/tictactoe/Game.js
@@ -1,39 +1,69 @@
-import React, {useState} from 'react';
+import React, {useReducer} from 'react';
 import Board from './Board';
 import './GameStyle.css';
 import {calculateWinner} from '../../helpers';
 
+const initialState = {
+  board: Array(9).fill(null),
+  xIsNext: true,
+};
+
+const gameReducer = (state, action) => {
+  switch (action.type) {
+    case 'CLICK': {
+      const {board, xIsNext} = state;
+      const {index, winner} = action.payload;
+
+      if (winner || board[index]) return state;
+
+      const nextState = JSON.parse(JSON.stringify(state));
+      nextState.board[index] = xIsNext ? 'X' : 'O';
+      nextState.xIsNext = !xIsNext;
+
+      return nextState;
+    }
+    case 'RESET': {
+      const nextState = JSON.parse(JSON.stringify(state));
+      nextState.board = Array(9).fill(null);
+      nextState.xIsNext = true
+      return nextState;
+    }
+
+    default:
+      break;
+  }
+  return state;
+};
+
 const Game = () => {
-  const [board, setBoard] = useState(Array(9).fill(null));
-  const [xIsNext, setXIsNext] = useState(true);
-  const winner = calculateWinner(board);
-  const handleClick = (i) => {
-    const boardCopy = [...board];
-    if (winner || boardCopy[i]) return;
-    boardCopy[i] = xIsNext ? 'X' : 'O';
-    setBoard(boardCopy);
-    setXIsNext((xIsNext) => !xIsNext);
+  const [state, dispatch] = useReducer(gameReducer, initialState);
+
+  const winner = calculateWinner(state.board);
+  const handleClick = (index) => {
+
+    dispatch({
+      type: 'CLICK', payload: {
+        index,
+        winner,
+      },
+    });
   };
   const handleResetGame = () => {
-    setBoard(Array(9).fill(null));
-    setXIsNext(true);
+    dispatch({
+      type: 'RESET',
+    });
   };
 
-  return (
-    <div>
-      <Board cells={board} onClick={handleClick}></Board>
-      {
-        winner &&
-        <h1 className="winner">
-          Winner is {winner}
-        </h1>
-      }
+  return (<div>
+    <Board cells={state.board} onClick={handleClick}></Board>
+    {winner && <h1 className="winner">
+      Winner is {winner}
+    </h1>}
 
 
-      <button className="btn-reset" onClick={handleResetGame}>Reset Game
-      </button>
-    </div>
-  );
+    <button className="btn-reset" onClick={handleResetGame}>Reset Game
+    </button>
+  </div>);
 };
 
 export default Game;
